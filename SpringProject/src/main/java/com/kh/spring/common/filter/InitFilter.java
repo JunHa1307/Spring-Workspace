f6edda3a7c5f0bd15417325d8e7bf36d16300e78
JunHa1307 <wnsgk1207@gmail.com>
2023-04-28 15:15:05 +0900

    update

--- SpringProject/src/main/java/com/kh/spring/common/filter/InitFilter.java
+++ SpringProject/src/main/java/com/kh/spring/common/filter/InitFilter.java
@@ -1,7 +1,6 @@
 package com.kh.spring.common.filter;
 
 import java.io.IOException;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -14,15 +13,15 @@ import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * Servlet Filter implementation class InitFilter
+ * Servlet Filter implementation class initFilter
  */
 @WebFilter(filterName="initFilter", urlPatterns = "/*")
-public class InitFilter extends HttpFilter implements Filter {
-
+public class initFilter extends HttpFilter implements Filter {
+	
     /**
      * Default constructor. 
      */
-    public InitFilter() {
+    public initFilter() {
         // TODO Auto-generated constructor stub
     }
 
@@ -37,17 +36,15 @@ public class InitFilter extends HttpFilter implements Filter {
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// contextPath 경로 저장 (/spring) ==> application 스코프에 저장할 예정
 		
-		// contextPath 경로 저장(/spring) --> application scope에 저장할 예정
-		
-		// 1. application 내장객체 얻어오기
+		// 1. application 내장 객체 얻어오기
 		ServletContext application = request.getServletContext();
 		
-		// 2. 최상위 주소 얻어오기(/spring)
+		// 2. 최상위 주소 얻어오기
 		String contextPath = ((HttpServletRequest) request).getContextPath();
 		
-		// 3. 세팅하기 --> application 스코프에 contextpath경로 저장
-		
+		// 3. 세팅하기 --> application 스코프에 contextpath 경로 저장
 		application.setAttribute("contextPath", contextPath);
 		
 		chain.doFilter(request, response);
--- SpringProject/src/main/java/com/kh/spring/member/model/vo/Member.java
+++ SpringProject/src/main/java/com/kh/spring/member/model/vo/Member.java
@@ -4,39 +4,44 @@ import java.sql.Date;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+
 /*
+ * lombok
  * - 자동 코드 생성 라이브러리
- * - 반복되는 getter, setter , toString 생성자 메소드 작성 등을 줄여주는 역할의 코드 라이브러리
+ * - 반복되는 getter, setter, toString, 생성자 메소드 작성등을 줄여주는 역할의 코드 라이브러리
  * 
- * lombok 설치 방법
- * 1. 라이브러리 다운 후 pom.xml 추가
- * 2. 다운로드된 jar 파일 찾아서 실행 (IDE 실행 중일 시 오류)
- * 3. IDE 재실행
+ * lombok 설치방법
+ * 1. 라이브러리 다운 후 pom.xml에 추가
+ * 2. 다운로드된 jar파일을 찾아서 실행.(ide가 켜져있으면 안됨)
+ * 3. ide 재실행
  * 
- * lombok 사용 시 주의사항
- * - uName, bTitle과 같이 앞글자가 소문자외자인 필드명은 만들면 안된다
- * - 필드명 작성시 소문자 두들자 이상으로 시작해야함
- * - el 표기법 사용시 내부적으로 getter 메소드를 찾게 되는데 이 때 getuName(), getbTitle()이라는 이름으로 메소드를 호출하기 때문
- * 다른 프로젝트를 컴퓨터에 옮길 때 컴퓨터에서 먼저 lombok을 실행 시키자.
-@NoArgsConstructor // 기본생성자
+ * lombok 사용시 주의사항
+ * - uName , bTitle과 같이 앞글자가 소문자외자인 필드명은 만들면 안됨
+ * - 필드명 작성시 소문자 두글자 이상으로 시작해야함.
+ * - el표기법 사용시 내부적으로 getter 메소드를 찾게 되는데, 이때 getuName(), getbTitle()이라는 이름으로
+ * 		메소드를 호출하기 때문에
+ * */
+
+/*@NoArgsConstructor // 기본 생성자
 @AllArgsConstructor // 모든 필드를 매개변수로 갖는 생성자
-@Setter // setter 메서드 자동 생성
-@Getter // getter 메서드 자동 생성
-@ToString // ToString 자동생성
-@EqualsAndHashCode // equals, hashcode 자동생성
-@Data // 저기서부터 여기까지 다주세요
-*/
-@Setter // setter 메서드 자동 생성
-@Getter // getter 메서드 자동 생성
-@ToString // ToString 자동생성
-@NoArgsConstructor // 기본생성자
-@AllArgsConstructor // 모든 필드를 매개변수로 갖는 생성자
-@Builder // Member m = new Member.builder().address("추가할 값").userId("추가할 값").build();
+//특정 필드 빼려면 final키워드 사용
+@Setter // setter메서드 자동 생성
+@Getter // getter메서드 자동 생성
+@ToString // toString 자동생성
+@EqualsAndHashCode*/ // equals, hashcode자동생성
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Member {
 	private int userNo;
 	private String userId;
@@ -48,4 +53,6 @@ public class Member {
 	private Date modifyDate;
 	private String status;
 	private String profileImage;
+	
+	
 }
