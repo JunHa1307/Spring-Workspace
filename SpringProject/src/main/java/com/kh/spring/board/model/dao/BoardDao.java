53cae5f11ea9799718f94fbc3ed0aa176171cf71
JunHa1307 <wnsgk1207@gmail.com>
2023-05-01 15:17:48 +0900

    update

--- SpringProject/src/main/java/com/kh/spring/board/model/dao/BoardDao.java
+++ SpringProject/src/main/java/com/kh/spring/board/model/dao/BoardDao.java
@@ -1,12 +1,16 @@
 package com.kh.spring.board.model.dao;
 
 import java.util.ArrayList;
+import java.util.Map;
 
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.kh.spring.board.model.vo.Board;
 import com.kh.spring.board.model.vo.BoardType;
+import com.kh.spring.common.model.vo.PageInfo;
 
 @Repository
 public class BoardDao {
@@ -18,4 +22,33 @@ public class BoardDao {
 		return (ArrayList) sqlSession.selectList("boardMapper.selectBoardTypeList");
 	}
 	
+	public int selectBoardListCount(String boardCode) {
+		return sqlSession.selectOne("boardMapper.selectBoardListCount",boardCode);
+	}
+	
+	public ArrayList<Board> selectBoardList(PageInfo pi, String boardCode) {
+		int offset = (pi.getCurrentPage()-1) * pi.getBoardLimit();
+		int limit = pi.getBoardLimit();
+		RowBounds rowBounds = new RowBounds(offset,limit);
+		return (ArrayList) sqlSession.selectList("boardMapper.selectBoardList", boardCode, rowBounds);
+	}
+	
+	public Board selectBoardDetail(int boardNo) {
+		return sqlSession.selectOne("boardMapper.selectBoardDetail",boardNo);
+	}
+	
+	public int updateReadCount(int boardNo) {
+		return sqlSession.update("boardMapper.updateReadCount",boardNo);
+	}
+
+	public int selectBoardSearchCount(Map<String, Object> paramMap) {
+		return sqlSession.selectOne("boardMapper.selectBoardSearchCount",paramMap);
+	}
+	
+	public ArrayList<Board> selectBoardSearch(PageInfo pi, Map<String, Object> paramMap) {
+		int offset = (pi.getCurrentPage()-1) * pi.getBoardLimit();
+		int limit = pi.getBoardLimit();
+		RowBounds rowBounds = new RowBounds(offset,limit);
+		return (ArrayList) sqlSession.selectList("boardMapper.selectBoardSearch", paramMap, rowBounds);
+	}
 }
--- SpringProject/src/main/java/com/kh/spring/board/model/service/BoardServiceImpl.java
+++ SpringProject/src/main/java/com/kh/spring/board/model/service/BoardServiceImpl.java
@@ -1,12 +1,16 @@
 package com.kh.spring.board.model.service;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.spring.board.model.dao.BoardDao;
+import com.kh.spring.board.model.vo.Board;
 import com.kh.spring.board.model.vo.BoardType;
+import com.kh.spring.common.model.vo.PageInfo;
+import com.kh.spring.common.template.Pagination;
 
 @Service
 public class BoardServiceImpl implements BoardService{
@@ -14,7 +18,44 @@ public class BoardServiceImpl implements BoardService{
 	@Autowired
 	private BoardDao boardDao;
 	
+	@Autowired
+	private Pagination pagination;
+	
 	public ArrayList<BoardType> selectBoardTypeList(){
 		return boardDao.selectBoardTypeList();
+	}
+	
+	public void selectBoardList(int cp, String boardCode, Map<String, Object> map) {
+		// 2) 페이지네이션 객체 생성
+		// 3) 게시글 목록 조회
+		int listCount = boardDao.selectBoardListCount(boardCode);
+		int pageLimit = 10;
+		int boardLimit = 5;
+		PageInfo pi = pagination.getPageInfo(listCount, cp, pageLimit, boardLimit);
+		
+		ArrayList<Board> list = boardDao.selectBoardList(pi, boardCode);
+		map.put("pi", pi);
+		map.put("list", list);
+		
+	}
+	
+	// 게시글 상세조회 구현 서비스
+	public Board selectBoardDetail(int boardNo) {
+		return boardDao.selectBoardDetail(boardNo);
+	}
+	
+	// 조회수 증가 서비스
+	public int updateReadCount(int boardNo) {
+		return boardDao.updateReadCount(boardNo);
+	}
+	
+	public void selectBoardSearch(int currentPage, Map<String, Object> paramMap, Map<String, Object> map) {
+		int listCount = boardDao.selectBoardSearchCount(paramMap);
+		int pageLimit = 10;
+		int boardLimit = 5;
+		PageInfo pi = pagination.getPageInfo(listCount, currentPage, pageLimit, boardLimit);
+		ArrayList<Board> list = boardDao.selectBoardSearch(pi, paramMap);
+		map.put("pi", pi);
+		map.put("list", list);
 	}
 }
